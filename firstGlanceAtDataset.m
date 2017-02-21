97142a4b3625e3192b8acddd33e6c75612ac7fc2
Peter Rupprecht <peter.rupprecht@fmi.ch>
2017-02-21 14:49:32 +0100

    Update files

--- firstGlanceAtDataset.m
+++ firstGlanceAtDataset.m
@@ -1,28 +1,25 @@
 % example matlab script for loading spikefinder data
-%
 % for more info see https://github.com/codeneuro/spikefinder
+% modified and enhanced by Peter Rupprecht (2017), ptrrupprecht.wordpress.com
 
-
-% load dataset
-dataset = '7';
+% load dataset; normally, there are 1:10 datasets; here, only one dataset is included in the repository
+dataset = '1';
 calcium_train = csvread([dataset '.train.calcium.csv']);
 spike_train = csvread([dataset '.train.spikes.csv']);
 
 % plot example neuron
-figure;
-n = 5; % index of neuron
-t = (0:length(calcium_train(:,n))-1)/100;     % 100Hz sampling rate, each bin 10 ms
-plot(t,zscore(calcium_train(:,n)),'k'); hold on
-plot(t,spike_train(:,5)-2,'r')
-xlim([t(1) 400]); ylim([-2.5 7]); xlabel('Time (s)'); ylabel('Fluorescence / Spike rate')
-legend('calcium','spikes')
+% figure;
+% n = 5; % index of neuron
+% t = (0:length(calcium_train(:,n))-1)/100;     % 100Hz sampling rate, each bin 10 ms
+% plot(t,zscore(calcium_train(:,n)),'k'); hold on
+% plot(t,spike_train(:,5)-2,'r')
+% xlim([t(1) 400]); ylim([-2.5 7]); xlabel('Time (s)'); ylabel('Fluorescence / Spike rate')
+% legend('calcium','spikes')
 
 
-
-%% load dataset
-counter = 1;
-for j = 1:10
-    j
+%% load dataset and calculate filter / calcium response function using a cross-correlation function between spikes and calcium traces
+counter = 1; clear filterXX CCC
+for j = 1 % datasets : normally, there are 1:10 datasets; here, only one dataset is included in the repository
     dataset = num2str(j);
     calcium_train = csvread([dataset '.train.calcium.csv']);
     spike_train = csvread([dataset '.train.spikes.csv']);
@@ -40,7 +37,7 @@ for j = 1:10
 
         filterX = ffx((numel(L_trace)-20):(numel(L_trace)+250)); % window of 2 sec
         filterX(21) = (filterX(20)+filterX(22))/2;
-        %         filterXX(counter,:) = (filterX - mean(filterX))/std(filterX);
+        filterXX(counter,:) = (filterX - mean(filterX))/std(filterX);
         filterXXX = nanmean(filterXX);
         
         prediction = conv(S_trace,filterX,'same');
@@ -54,13 +51,14 @@ for j = 1:10
             F(k) = corr(filterX,L_trace(k:(k+numel(filterX)-1)));
         end
         
-        F = deconv(filterX,L_trace);
-        figure, plot(F)
-        
         counter = counter + 1;
     end
 end
-figure, imagesc(filterXX)
+% plot the filters
+figure, imagesc(([1:250]-20)/100,[],filterXX);
+xlabel('time (sec)');
+ylabel('neuron index');
+
 
 figure(243)
 for j = 1:10
